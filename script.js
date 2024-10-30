88b80b38b77d0b75c3ed2be93fef3cc1b70710ab
minhle8103 <91530654+minhle8103@users.noreply.github.com>
2024-10-30 18:03:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -6,3 +7,23 @@ menuOpenButton.addEventListener("click", ()=>
 );
 
 menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=>menuOpenButton.click());
+});
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
